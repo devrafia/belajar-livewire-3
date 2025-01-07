3494abbe32868c21220af5b52f53422c52440e21
rafia <akmalrafi1132@gmail.com>
2025-01-07 13:48:04 +0700

    membuat judul dinamis

---
+++ resources/views/livewire/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Contact: A good traveler has no fixed plans and is not intent upon arriving.
+</div>
---
+++ resources/views/livewire/posts/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Posts: If you look to others for fulfillment, you will never truly be fulfilled.
+</div>
